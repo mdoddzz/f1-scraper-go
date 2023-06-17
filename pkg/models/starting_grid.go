d03b426ad547fa73f588111c2dcd86edd664d0b4
Mike Dodd <mike@wilxite.com>
2023-06-17 16:45:42 +0100

    Also updating starting grid model

--- pkg/models/starting_grid.go
+++ pkg/models/starting_grid.go
@@ -1,16 +1,14 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 // StartingGrid : model for the race starting grid
 type StartingGrid struct {
-	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	RaceId   primitive.ObjectID `json:"race_id" bson:"race_id"`
-	Position int                `json:"position" bson:"position"`
-	Number   int                `json:"number" bson:"number"`
-	Driver   string             `json:"driver" bson:"driver"`
-	Car      string             `json:"car" bson:"car"`
-	Time     F1Time             `json:"time" bson:"time"`
+	ID       string `json:"id" bson:"_id,omitempty"`
+	RaceId   string `json:"race_id" bson:"race_id"`
+	Position int    `json:"position" bson:"position"`
+	Number   int    `json:"number" bson:"number"`
+	Driver   string `json:"driver" bson:"driver"`
+	Car      string `json:"car" bson:"car"`
+	Time     F1Time `json:"time" bson:"time"`
 }
 
 // StartingGridService : interface for the starting grid model
