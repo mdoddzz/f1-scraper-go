14c850d4012dd64c5f06a02f9dcbcc9ce07f7a64
Mike Dodd <mike@wilxite.com>
2023-06-20 16:14:29 +0100

    Updated URLs to handle race sidebar

--- pkg/scraper/urls.go
+++ pkg/scraper/urls.go
@@ -12,8 +12,23 @@ func (s *service) CollectURLs(startURL string) {
 	// Set url array
 	archive_urls := []string{startURL}
 
-	// Set get archive URLs
+	// Get the URLS from links at top
 	s.col.OnHTML(".resultsarchive-filter-item-link", func(e *colly.HTMLElement) {
+
+		// Get url from archive link
+		url := e.Attr("href")
+
+		// Check if URL is new
+		if !slices.Contains(archive_urls, url) {
+			fmt.Println("Found New Archive URL ", e.Attr("href"))
+			s.col.Visit(s.baseURL + url)
+			archive_urls = append(archive_urls, url)
+		}
+
+	})
+
+	// Get URLs from sidebar
+	s.col.OnHTML(".side-nav-item-link", func(e *colly.HTMLElement) {
 
 		// Get url from archive link
 		url := e.Attr("href")
